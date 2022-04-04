95c2fbd910047f857db8adb7155ffb6cd93388f9
happex <98713994+nao-l@users.noreply.github.com>
2022-04-04 18:25:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,12 @@
 		fetch("JSON.json")
 		.then(response => response.json())
 		.then(data => {
-			
+			var name = data[1].name;
+			var age = data[0].age;
+			const content = `
+			<span style="color:#FF0000">${name}</span><br>
+			<span style="color:#00FF00">${age}</span>`
+			document.getElementById("body").innerHTML = content;
 		})
 	}
 })
